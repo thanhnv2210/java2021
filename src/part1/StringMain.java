149a1eb9547f8533bab3b634029b8cfe448477d8
david <thanh@moneytap.vn>
2021-11-03 09:54:32 +0700

    Test annotation

--- src/part1/StringMain.java
+++ src/part1/StringMain.java
@@ -1,8 +1,21 @@
 package part1;
 
+import org.junit.jupiter.api.Test;
+
 public class StringMain {
     public static void main(String[] args) {
         String input = "First Value";
-        System.out.println(input);
+        System.out.printf("%s:%s%n", input, input.hashCode());
+        input = "Second Value";
+        System.out.printf("%s:%s%n", input, input.hashCode());
+    }
+
+    @Test
+    public void testAnnotation(){
+        assert getCompanyName().equals("moneytap");
+    }
+
+    private static String getCompanyName(){
+        return "moneytap";
     }
 }
